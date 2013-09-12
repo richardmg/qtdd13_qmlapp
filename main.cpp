9da7f80720aaf7a2e04c3cb08c79e99e6fee8697
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2013-09-12 11:45:07 +0200

    icons

---
+++ main.cpp
@@ -0,0 +1,22 @@
+
+
+#include <QtQuick>
+
+Q_IMPORT_PLUGIN(QtQuick2Plugin)
+Q_IMPORT_PLUGIN(QtSensorsDeclarativeModule)
+
+int main(int argc, char **argv)
+{
+    QGuiApplication app(argc, argv);
+
+    qputenv("QML2_IMPORT_PATH", "qml");
+
+    QQuickView view(QUrl("qrc:/main.qml"));
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+
+    view.show();
+
+    return app.exec();
+}
+
+
---
+++ main.qml
@@ -0,0 +1,18 @@
+import QtQuick 2.1
+
+Rectangle {
+    id: mainWindow
+    color: "white"
+
+    Image {
+        x: 200
+        y: 200
+        source: "qrc:/qt.png"
+    }
+
+    Image {
+        x: 400
+        y: 400
+        source: "qrc:/qt.png"
+    }
+}
