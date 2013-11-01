e63b5047572390c36da6913e766c88432e078fa8
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2013-11-01 13:36:52 +0100

    init resource

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char **argv)
 {
+    Q_INIT_RESOURCE(scenegraph);
     QGuiApplication app(argc, argv);
     app.primaryScreen()->setOrientationUpdateMask(Qt::PortraitOrientation | Qt::LandscapeOrientation);
 
